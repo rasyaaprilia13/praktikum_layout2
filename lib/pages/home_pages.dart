d2f4df0c03400bf5ad7836afdbce37d6d60da0e2
rasyaaprilia13 <rassyaapr13@gmail.com>
2025-09-09 22:08:01 +0700

    Update home_pages.dart

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -4,39 +4,98 @@ import '../models/item.dart';
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
-  // data dummy
   final List<Item> items = [
-    Item(name: 'Milk', price: 7000),
-    Item(name: 'Instant Noodle', price: 3000),
+    Item(name: "Ketcup", price: 70000, image: "assets/kecap.jpg", stock: 10, rating: '4.5'),
+    Item(name: "LPG Gas", price: 17000, image: "assets/gas.jpg", stock: 20, rating: '4.8'),
+    Item(name: "SariRoti Bread", price: 18000, image: "assets/roti.jpg", stock: 15, rating: '4.6'),
+    Item(name: "Cheese", price: 30000, image: "assets/keju.jpg", stock: 25, rating: '4.7'),
+   
   ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Shopping List"),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-      ),
-      body: ListView.builder(
+      appBar: AppBar(title: const Text("RasyaMart")),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+        ),
         itemCount: items.length,
         itemBuilder: (context, index) {
           final item = items[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-            child: ListTile(
-              title: Text(item.name),
-              trailing: Text(item.price.toString()),
-              onTap: () {
-                Navigator.pushNamed(
-                  context,
-                  '/item',
-                  arguments: item, // kirim data item ke halaman ItemPage
-                );
-              },
-            ),
-          );
+          return ItemCard(item: item);
         },
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Nama: [Rasya Aprilia] - NIM: [362458302115]',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Item item;
+
+  const ItemCard({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, '/item', arguments: item);
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Hero(
+                tag: 'itemPhoto${item.name}',
+                child: Image.asset(
+                  item.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                item.name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text("Rp ${item.price}"),
+            ),
+            Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+             child: Text("Stok: ${item.stock}"),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 8.0),
+              child: Row(
+                children: [
+                  const Icon(Icons.star, color: Colors.amber, size: 20),
+                  const SizedBox(width: 4),
+                  Text(item.rating.toString()),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
