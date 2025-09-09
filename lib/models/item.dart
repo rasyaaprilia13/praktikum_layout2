158c0ea2960f8d12dc0536d142110e9a85d13acf
rasyaaprilia13 <rassyaapr13@gmail.com>
2025-09-09 22:08:46 +0700

    Update item.dart

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,9 +1,15 @@
 class Item {
   final String name;
   final int price;
+  final int stock;
+  final String image;
+  final String rating;
 
   Item({
     required this.name,
     required this.price,
+    required this.stock,
+    required this.image,
+    required this.rating,
   });
 }
